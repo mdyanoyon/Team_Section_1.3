8a34cbfc5d16bd48c2e1c623b2781d18c7e693af
Y.A. Noyon <65534295+mdyanoyon@users.noreply.github.com>
2022-03-24 12:43:54 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // init: false,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView:1.2,
+      spaceBetween: 20,
+    },
+    768: {
+      spaceBetween: 25,
+    },
+    1024: {
+      spaceBetween: 30,
+    },
+  }
+});
